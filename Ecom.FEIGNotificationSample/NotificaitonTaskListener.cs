4ef9fa8eebafe3e2c0d1c781eaa6dee57a194d44
Ryan Blackmore <ryan@ryanblackmore.com>
2017-10-11 10:36:47 +1100

    Complete

---
+++ Ecom.FEIGNotificationSample/NotificaitonTaskListener.cs
@@ -0,0 +1,118 @@
+﻿using OBID;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.FEIGNotificationSample
+{
+    public class NotificaitonTaskListener : FedmTaskListener
+    {
+        #region Private Attributes
+
+        private FedmIscReader _reader;
+
+        #endregion
+
+        #region Constructors
+
+        public NotificaitonTaskListener(FedmIscReader reader)
+        {
+            _reader = reader;
+        }
+        #endregion
+
+        //Executes when a new notificaiton is recieved from the reader.
+        public void OnNewNotification(int iError, string ip, uint portNr)
+        {
+            //Get Array of BRM Items from BRM Table in reader object
+            FedmBrmTableItem[] tagReads = (_reader.GetTable(FedmIscReaderConst.BRM_TABLE) as FedmBrmTableItem[]);
+
+            if (tagReads != null && tagReads.Length > 0)
+            {
+                Console.WriteLine($"------------------------");
+                Console.WriteLine($"New Notifiaiton Recieved");
+
+
+                foreach (FedmBrmTableItem item in tagReads)
+                {
+                    Console.WriteLine($"Tag ID: {item.GetUid()}");
+                    //Check if Time Information is present
+                    if (item.isTimer)
+                    {
+                        FeIscReaderTime timer = item.GetReaderTime();
+
+                        int ms = (timer.MilliSecond % 1000);
+                        int second = (timer.MilliSecond - ms) / 1000;
+
+                        Console.WriteLine($"Time: {timer.Year}-{timer.Month}-{timer.Day}T{timer.Hour}:{timer.Minute}:{second}:{ms}");
+                    }
+
+                    Console.Write($"Antenna Values: ");
+
+                    //Check if RSSI Data is available (this includes the antenna port number on the MUX
+                    if (item.isRSSI)
+                    {
+                        foreach (var rssi in item.GetRSSI())
+                        {
+                            Console.Write($"{rssi.Value.antennaNumber} ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine($"End of Notification");
+                    Console.WriteLine($"-------------------");
+                    Console.WriteLine();
+
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        #region Unused Method Implementations from FedmTaskListener
+        public void OnNewApduResponse(int iError)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public void onNewPeopleCounterEvent(uint counter1, uint counter2, uint counter3, uint counter4, string ip, uint portNr, uint busAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNewQueueResponse(int iError)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNewReaderDiagnostic(int iError, string ip, uint portNr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNewSAMResponse(int iError, byte[] responseData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNewTag(int iError)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
--- Ecom.FEIGNotificationSample/Program.cs
+++ Ecom.FEIGNotificationSample/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OBID;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,36 @@ namespace Ecom.FEIGNotificationSample
 {
     class Program
     {
+
+        private static FedmIscReader _reader;
+
         static void Main(string[] args)
         {
+
+            _reader = new FedmIscReader();
+
+            //Initilize internal table size (default is 0, without initilization, there is no table to place data coming in)
+            _reader.SetTableSize(FedmIscReaderConst.BRM_TABLE, 255);
+
+
+            //Create a new FedmTaskListener object, this Interface implements the OnNewNotificaiton() method, which will fire when the reader sends through a new notificaiton
+            FedmTaskListener listener = new NotificaitonTaskListener(_reader);
+
+            //Set up FedmTaskOption, this is where you configure the port to listen to, as well as the option to reply with an acknoledgement
+            FedmTaskOption taskOpt = new FedmTaskOption()
+            {
+                IPPort = 10005,
+                NotifyWithAck = 0 // 0 = Off, 1 = Off
+            };
+
+            Console.WriteLine("Begin Listening");
+            Console.WriteLine();
+
+            //Initialize notificaiotn listening, by starting a new background task
+            _reader.StartAsyncTask(FedmTaskOption.ID_NOTIFICATION,listener,taskOpt);
+
+            Console.ReadKey();
         }
+
     }
 }
